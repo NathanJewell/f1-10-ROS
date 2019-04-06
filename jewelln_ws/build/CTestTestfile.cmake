c2d2dcb31911ce710cf53645ecf8abe884e4f71f
Nathan Jewell <ncjewell@gmail.com>
2019-04-06 15:39:48 -0700

    part 1 (tutorial) + 2 (bags)

---
+++ jewelln_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nathan/test/src
+# Build directory: /home/nathan/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ jewelln_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/nathan/test/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/nathan/test/devel;/opt/ros/melodic".split(';') if "/home/nathan/test/devel;/opt/ros/melodic" != "" else []
